ecbaf70847ad5015c672bfce927f80ceb55a5e2a
‪Ahmed Aly Mohamed <ahmedalimohamed1994@gmail.com>
2020-12-20 19:12:10 +0200

    working on sidebar

--- js/layout.js
+++ js/layout.js
@@ -12,3 +12,17 @@ $(".mid-header-user-container .profile-container").mouseleave(
         menu.fadeOut(400);
     }
 );
+
+// Sidebar styles script
+
+$(".logo-and-sidebar-container .sidebar-button").click(function () {
+    $("#sidebar").css("width", "18vw");
+    setTimeout(() => {
+        $(".sidebar-container").fadeIn(100);
+    }, 350);
+});
+
+$(".sidebar-collapse-button").click(function () {
+    $(".sidebar-container").fadeOut(100);
+    $("#sidebar").css("width", "0");
+});
