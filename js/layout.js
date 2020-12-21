b792ca74a3db137d530e9e35475907710a0c1523
‪Ahmed Aly Mohamed <ahmedalimohamed1994@gmail.com>
2020-12-21 18:13:07 +0200

    working on sidebar functionalities

--- js/layout.js
+++ js/layout.js
@@ -26,3 +26,35 @@ $(".sidebar-collapse-button").click(function () {
     $(".sidebar-container").fadeOut(100);
     $("#sidebar").css("width", "0");
 });
+
+// sub menu scripts
+
+$(".sub-menu-toggler").mouseenter(function () {
+    $(".main-sub-menu", this).fadeIn(400);
+});
+
+$(".sub-menu-toggler").mouseleave(function () {
+    $(".main-sub-menu", this).fadeOut(350);
+});
+
+// Scripts for sidebar link toggler sub menu
+
+$(".sidebar-link-toggler").click(function () {
+    var nextElement = $(this).next();
+    if (nextElement.hasClass("sidebar-sub-link-container")) {
+        if (nextElement.css("display") == "none") {
+            nextElement.show(300);
+            setTimeout(() => {
+                $(".sidebar-sub-link", nextElement).css("visibility", "visible");
+            }, 300);
+        }
+        else {
+            setTimeout(() => {
+                $(".sidebar-sub-link", nextElement).css("visibility", "hidden");
+            }, 100);
+            setTimeout(() => {
+                nextElement.hide(300);
+            }, 300);
+        }
+    }
+});
