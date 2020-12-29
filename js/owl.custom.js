80c646e2566c99b8b634a65e2b5547a24ffbbd4a
‪Ahmed Aly Mohamed <ahmedalimohamed1994@gmail.com>
2020-12-29 15:32:59 +0200

    owl carousel works properly v3

--- js/owl.custom.js
+++ js/owl.custom.js
@@ -1,23 +1,34 @@
-$(".section-carousel").owlCarousel({
-    margin: 30,
-    loop: false,
-    autoplay: false,
-    autoplayTimeout: 5000,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        768: {
-            items: 2
-        },
-        992: {
-            items: 3
-        },
-        1200: {
-            items: 5.5
+$(document).ready(function () {
+
+    $(".section-carousel").owlCarousel({
+        margin: 30,
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 7000,
+        responsive: {
+            0: {
+                items: 1.25
+            },
+
+            600: {
+                items: 2
+            },
+
+            768: {
+                items: 2.5
+            },
+            992: {
+                items: 3.5
+            },
+            1200: {
+                items: 4
+            }
         }
-    }
+    });
+
+    var owlItems = $(".section-carousel .owl-item").length;
+    console.log("we are " + owlItems + " owl items.")
+
+    $(".section-carousel").trigger("to.owl.carousel", [1, 1]);
+
 });
